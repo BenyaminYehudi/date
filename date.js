014718a2f60bb9faf2d660cfb06c8fd8198cfab8
BenyaminYehudi <benyamin.yehudi@gmail.com>
2014-11-17 16:11:22 +0200

    Update date.js

--- date.js
+++ date.js
@@ -1,10 +1,11 @@
 function date(){
     var daydate = new Date();
-    var n = daydate.getDate();
+    var n = daydate.toString();
     document.getElementById("todaysDate").innerHTML = n;
     var day = new Date();
     var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     document.getElementById("todaysDate").innerHTML = days[dday.getDay()];
     var year = new Date();
     document.getElementById("todaysDate").innerHTML = year.getFullYear();
+    
 }
